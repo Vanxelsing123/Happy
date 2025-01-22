b2ec4198ecce70cd8608c0a6d48ba63c8cd78569
Vladislav Shemet <135120438+Vanxelsing123@users.noreply.github.com>
2025-01-22 15:22:41 +0300

    Add files via upload

---
+++ JS/script.js
@@ -0,0 +1,44 @@
+const swiper = new Swiper(".mySwiper", {
+    spaceBetween: 30,
+    centeredSlides: true,
+    autoplay: {
+      delay: 3500,
+      disableOnInteraction: false,
+    },
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+  });
+
+  let accordion = document.querySelector('.accordion');
+let items = accordion.querySelectorAll('.accordion__item');
+let title = accordion.querySelectorAll('.accordion__title');
+
+function toggleAccordion() {
+  let thisItem = this.parentNode;
+  
+  items.forEach(item => {
+    if (thisItem == item ) {
+      // if this item is equal to the clicked item, open it.
+      thisItem.classList.toggle('active');
+      return;
+    } 
+    // otherwise, remove the open class
+    item.classList.remove('active');
+  });
+}
+
+title.forEach(question => question.addEventListener('click', toggleAccordion));
+
+$(function() {
+  $('.marquee').marquee({
+    duration: 7000,
+    startVisible: true,
+    duplicated: true
+  });
+});
