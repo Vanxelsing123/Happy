6dd53b66dbbe5cb2443f5fc245f47d54b9a78c15
Vladislav Shemet <135120438+Vanxelsing123@users.noreply.github.com>
2025-02-10 20:17:38 +0300

    Add files via upload

--- JS/script-about.js
+++ JS/script-about.js
@@ -9,3 +9,27 @@ const swiper = new Swiper(".mySwiper", {
       prevEl: ".swiper-button-prev",
     },
   });
+
+  const burger = document.querySelector('.burger');
+const header__nav = document.querySelector('.header__menu--top');
+const header__item = header__nav.querySelectorAll('a');
+const body = document.body;
+
+burger.addEventListener('click', () => {
+    body.classList.toggle('stop-scroll');
+    burger.classList.toggle('burger--active');
+    header__nav.classList.toggle('header__menu--visible');
+});
+
+header__item.forEach(el => {
+    el.addEventListener('click', () => {
+        body.classList.remove('stop-scroll');
+    burger.classList.remove('burger--active');
+    header__nav.classList.remove('header__menu--visible')
+    });
+});
+
+var swiper2 = new Swiper(".mySwiper-about-us", {
+  effect: "cards",
+  grabCursor: true,
+});
