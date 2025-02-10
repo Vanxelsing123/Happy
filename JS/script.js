2562eb943a36aa051efe8f4238e8dc61a575b9c3
Vladislav Shemet <135120438+Vanxelsing123@users.noreply.github.com>
2025-02-10 14:25:56 +0300

    Add files via upload

--- JS/script.js
+++ JS/script.js
@@ -15,6 +15,8 @@ const swiper = new Swiper(".mySwiper", {
     },
   });
 
+
+
   let accordion = document.querySelector('.accordion');
 let items = accordion.querySelectorAll('.accordion__item');
 let title = accordion.querySelectorAll('.accordion__title');
@@ -42,3 +44,92 @@ $(function() {
     duplicated: true
   });
 });
+
+
+/* select */
+
+document.addEventListener("DOMContentLoaded", function () {
+  const select = document.getElementById("standard-select");
+  const photos = document.querySelectorAll("#birthdays .photo");
+
+  select.addEventListener("change", function () {
+      const selectedCategory = select.value;
+
+      photos.forEach(photo => {
+          const category = photo.getAttribute("data-category");
+          if (selectedCategory === "all" || category === selectedCategory) {
+              photo.style.removeProperty("display");
+          } else {
+              photo.style.display = "none";
+          }
+          
+      });
+  });
+});
+
+
+/* burger */
+
+const burger = document.querySelector('.burger');
+const header__nav = document.querySelector('.header__menu--top');
+const header__item = header__nav.querySelectorAll('a');
+const body = document.body;
+
+burger.addEventListener('click', () => {
+    body.classList.toggle('stop-scroll');
+    burger.classList.toggle('burger--active');
+    header__nav.classList.toggle('header__menu--visible');
+});
+
+header__item.forEach(el => {
+    el.addEventListener('click', () => {
+        body.classList.remove('stop-scroll');
+    burger.classList.remove('burger--active');
+    header__nav.classList.remove('header__menu--visible')
+    });
+});
+
+const swiper2 = new Swiper(".mySwiper-about", {
+  spaceBetween: 30,
+  centeredSlides: true,
+  autoplay: {
+    delay: 3500,
+    disableOnInteraction: false,
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
+
+const swiper3 = new Swiper(".mySwiper-featured", {
+  spaceBetween: 30,
+  centeredSlides: true,
+  autoplay: {
+    delay: 3500,
+    disableOnInteraction: false,
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
+
+
+const swiper4 = new Swiper(".mySwiper-photo", {
+  effect: "coverflow",
+  grabCursor: true,
+  centeredSlides: true,
+  slidesPerView: "auto",
+  coverflowEffect: {
+    rotate: 50,
+    stretch: 0,
+    depth: 100,
+    modifier: 1,
+    slideShadows: true,
+  },
+  pagination: {
+    el: ".swiper-pagination",
+  },
+});
+
